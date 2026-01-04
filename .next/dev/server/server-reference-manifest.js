8afeeb72ed573fc8243075e76c1efc4653347ced
Francis Kodama <fk@fkodama.com>
2026-01-04 10:54:23 -0500

    No secrets

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZIY/IrrDy1p8WGGVY+j225gq6EOYQPmPxx/DQ1LhE5M=\"\n}"
---
+++ .next/dev/static/chunks/dev_portfolio-2_1_pages__app_2da965e7._.js
@@ -0,0 +1,15 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/4e0c1_next_dist_compiled_449379e3._.js",
+  "static/chunks/4e0c1_next_dist_shared_lib_13f3a2d8._.js",
+  "static/chunks/4e0c1_next_dist_client_b4831fc6._.js",
+  "static/chunks/4e0c1_next_dist_76658a00._.js",
+  "static/chunks/4e0c1_next_app_621d553c.js",
+  "static/chunks/[next]_entry_page-loader_ts_9b401583._.js",
+  "static/chunks/4e0c1_react-dom_b40edcce._.js",
+  "static/chunks/4e0c1_7de28c75._.js",
+  "static/chunks/[root-of-the-server]__36aecf60._.js"
+],
+    source: "entry"
+});
